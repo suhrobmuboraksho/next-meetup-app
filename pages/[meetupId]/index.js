4d8f3ad5e9a453a9bf81055dba14dccc7007986d
Suhrob <suhrob.m89@gmail.com>
2023-02-24 10:51:43 -0800

    Adding getStaticPaths() to the Meetup Page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,39 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 export default function MeetupDetails() {
-  return (
-    <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First Meetup"
-      address="Random address 34"
-      description="This is the First Meetup"
-    />
-  );
+  return <MeetupDetail image="" title="" address="" description=" " />;
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "First Meetup",
+        address: "Random address 34",
+        description: "This is the First Meetup",
+      },
+    },
+  };
 }
