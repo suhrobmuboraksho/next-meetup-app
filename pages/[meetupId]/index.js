799466bc2f5a9596345d967a2997d313efbda979
Suhrob Muborakshoev <suhrob.m89@gmail.com>
2023-02-25 22:16:09 -0800

    Fetching individual params & meetup

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,38 +1,61 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-export default function MeetupDetails() {
-  return <MeetupDetail image="" title="" address="" description=" " />;
+export default function MeetupDetails({ meetupData }) {
+  return (
+    <MeetupDetail
+      image={meetupData.image}
+      title={meetupData.title}
+      address={meetupData.address}
+      description={meetupData.description}
+    />
+  );
 }
 
 export async function getStaticPaths() {
+  const MONGO_API =
+    "mongodb+srv://suhrobmuboraksho:tOHnJF6lI8e6sIaX@cluster0.9vuaze9.mongodb.net/meetups?retryWrites=true&w=majority";
+  const client = await MongoClient.connect(MONGO_API);
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
+
+  const MONGO_API =
+    "mongodb+srv://suhrobmuboraksho:tOHnJF6lI8e6sIaX@cluster0.9vuaze9.mongodb.net/meetups?retryWrites=true&w=majority";
+  const client = await MongoClient.connect(MONGO_API);
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-        title: "First Meetup",
-        address: "Random address 34",
-        description: "This is the First Meetup",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
